6bd50f07e7a06501f64fb7c6dcb23d2c0e94445e
Ben March <bmarch89@gmail.com>
2017-12-21 14:38:35 -0500

    dist

--- dist/angular-bootstrap-tour.js
+++ dist/angular-bootstrap-tour.js
@@ -331,7 +331,7 @@
             } else if (attrs[helpers.getAttrName('templateUrl')]) {
                 lookupTemplate(attrs[helpers.getAttrName('templateUrl')], scope).then(function (template) {
                     if (template) {
-                        options.template = template.data;
+                        options.template = template;
                         deferred.resolve(template);
                     }
                 });
